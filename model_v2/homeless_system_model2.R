24179e1b1a4fae484b2c5f9650af9f34e4912939
matt5mitchell <mattsmitchell@gmail.com>
2022-05-13 23:30:57 -0700

    created stocastic model

--- model_v2/homeless_system_model2.R
+++ model_v2/homeless_system_model2.R
@@ -11,9 +11,6 @@ rm(list = ls(all = TRUE))  # resets R to fresh
 
 library(ggplot2)
 
-set.seed(126189)
-
-niter <- 50
 
 #### Deterministic model function ####
 model_func=function(t, x, vparameters){
@@ -70,7 +67,75 @@ vparameters = c(alpha = alpha, beta_u = beta_u, beta_s = beta_s, beta_p = beta_p
 inits <- c(U = U_0, S = S_0, P = P_0, O = O_0)
 dmodel <- as.data.frame(lsoda(inits, vt, model_func, vparameters))
 
-ggplot(dmodel, aes(x = time, y = U)) + 
-  geom_line() +
+# ggplot(dmodel, aes(x = time, y = U)) + 
+#   geom_line() +
+#   scale_x_continuous(breaks = seq(0, 10, 1)) +
+#   expand_limits(y = 0)
+
+#### Stochastic Model ####
+# Credit for approach and significant chunks of code to: Sherry Towers
+# http://sherrytowers.com/2016/02/06/stochastic-compartmental-modelling-with-stochastic-differential-equations-2/
+
+
+set.seed(78324)
+niter <- 100
+
+delta_t <- 1/12
+zstate <- list()
+i <- 1L
+for (iter in 1:niter){
+  time <- 0
+  vstate <- c(U_0, S_0, P_0, O_0)
+  
+  # State changes: http://sherrytowers.com/2016/01/02/stochastic-compartmental-modelling/#step2
+  K <- length(vstate)  # number of compartments
+  J <- 4               # number of possible state changes
+  lambda <- matrix(0,nrow=J,ncol=length(vstate))
+  lambda[1,] <- c(1,0,0,0)
+  lambda[2,] <- c(-1,0,0,1)
+  lambda[3,] <- c(-1,0,0,1)
+  lambda[4,] <- c(-1,0,0,1)
+  
+  while(vstate[1] > 0 & time < tend) {
+    zstate[[i]] <- c(vstate, time, iter)
+    U <- vstate[1]
+    S <- vstate[2]
+    P <- vstate[3]
+    O <- vstate[4]
+    vec_p <- c(alpha, 
+              beta_u*U,
+              beta_s*S,
+              beta_p*P)
+    
+    G_t <- lambda
+    for (irow in 1:nrow(G_t)){
+      G_t[irow,] <- G_t[irow,]*sqrt(vec_p[irow])
+    }
+    G <- t(G_t)
+    
+    W <- rnorm(ncol(G))
+    delta_U <- delta_t*(alpha - beta_u*U - beta_s*S - beta_p*P) + sqrt(delta_t)*sum(G[1,]*W)
+    delta_S <- 0 #delta_t*0                                     + sqrt(delta_t)*sum(G[2,]*W)
+    delta_P <- 0 #delta_t*0                                     + sqrt(delta_t)*sum(G[3,]*W)
+    delta_O <- delta_t*(beta_u*U + beta_s*S + beta_p*P)         + sqrt(delta_t)*sum(G[3,]*W)
+    
+    vstate[1] <- vstate[1] + delta_U
+    vstate[2] <- vstate[2] + delta_S 
+    vstate[3] <- vstate[3] + delta_P 
+    vstate[4] <- vstate[4] + delta_O 
+    
+    vstate[vstate<0] <- 0
+    i <- i + 1
+    time <- time + delta_t
+  }
+  cat("Doing SDE realisation:",iter,niter," ",time,vstate,"\n")
+}
+
+smodel <- data.frame(matrix(unlist(zstate), ncol = 6, byrow = TRUE))
+colnames(smodel) <- c("U", "S", "P", "O", "time", "iter")
+
+ggplot() +
+  geom_line(data = smodel, aes(x = time, y = U, group = iter), alpha = .1) +
+  geom_line(data = dmodel, aes(x = time, y = U), color = "indianred") +
   scale_x_continuous(breaks = seq(0, 10, 1)) +
   expand_limits(y = 0)
